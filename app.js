c7367d7f5edb18ee2d30195db848d02e870e769b
Christopher Wray <chris@sol.company>
2023-08-27 16:10:07 -0700

    create routes and dummy controllers

--- app.js
+++ app.js
@@ -7,12 +7,13 @@ require("dotenv").config();
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
+const catalogRouter = require("./routes/catalog");
 
 const app = express();
 
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const mongoDB = prosess.env.DATABASE_CONNECTION_STRING;
+const mongoDB = process.env.DATABASE_CONNECTION_STRING;
 
 main().catch((err) => console.log(err));
 async function main() {
@@ -31,6 +32,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/catalog", catalogRouter); // Add catalog routes to middleware chain.
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
